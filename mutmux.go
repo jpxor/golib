f4341bef3f1dc97015244560e4a20bacd384a1fe
Josh Simonot <jpxgit@outlook.com>
2018-01-25 23:33:03 -0500

    initial source

---
+++ mutmux.go
@@ -0,0 +1,56 @@
+// Package mutmux provides the ability to synchronize concurrent
+// accesses to a set of resources. Named for "Mutex Multiplexer".
+//
+// The original use case was to handle many go routines performing io
+// operations on a set of many files in undeterministic fashion.
+package mutmux
+
+import "sync"
+
+// Mutmux is a mutex multiplexer for synchronizing
+// access to a set of shared resources.
+type Mutmux struct {
+	mutex *sync.Mutex
+	xmap  map[string]*sync.Mutex
+}
+
+// Lock ensures mutual exclusion to the named resource
+// and provides a means of releasing the lock.
+type Lock struct {
+	resid *string
+	xmap  map[string]*sync.Mutex
+}
+
+// Init will initialize the Mutmux.
+func (s Mutmux) Init() Mutmux {
+	s.mutex = &sync.Mutex{}
+	s.xmap = make(map[string]*sync.Mutex)
+	return s
+}
+
+// GetLock attempts to acquire the lock for the named resource.
+// This function will block until the lock is acquired.
+func (s Mutmux) GetLock(resid string) Lock {
+	lock := s.xmap[resid]
+	if lock == nil {
+		s.mutex.Lock()
+		lock = s.xmap[resid]
+		if lock == nil {
+			lock = &sync.Mutex{}
+			s.xmap[resid] = lock
+		}
+		s.mutex.Unlock()
+	}
+	lock.Lock()
+	return Lock{
+		resid: &resid,
+		xmap:  s.xmap,
+	}
+}
+
+// Release will release the lock
+func (lock Lock) Release() {
+	lock.xmap[*lock.resid].Unlock()
+	lock.resid = nil
+	lock.xmap = nil
+}
